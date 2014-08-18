870256524eb563e481661dbb6071048c5a7d1e3a
Cristina <cristina@conekta.mx>
2014-08-18 09:33:38 -0500

    documentation fixes

--- images/js/script.js
+++ images/js/script.js
@@ -1,28 +1,19 @@
   jQuery(function($) {
-    Stripe.setPublishableKey('<?= $this->publishable_key ?>');
+    Conekta.setPublishableKey('<?= $this->publishable_key ?>');
     var $form = $('.checkout');
 
-    var stripeResponseHandler = function(status, response) {
 
-    if (response.error) {
-
-      // Show the errors on the form
-      $form.find('.payment-errors').text(response.error.message);
-      // Unblock the form to re-enter data
+var conektaErrorResponseHandler = function(response) {
+      $form.find('.payment-errors').text(response.message);
       $form.unblock();
-
-    } else {
-      // Append the Token
-      $form.append($('<input type="hidden" name="stripeToken" />').val(response.id));
-
-      //Re-Submit
+ };
+    var conektaResponseHandler = function(response) {
+      $form.append($('<input type="hidden" name="conektaToken" />').val(response.id));
       $form.submit();
-
-    }
   };
     $('body').on('click', '.checkout input:submit', function(){
       // Make sure there's not an old token on the form
-      $('.checkout').find('[name=stripeToken]').remove()
+      $('.checkout').find('[name=conektaToken]').remove()
     })
 
     // Bind to the checkout_place_order event to add the token
@@ -31,11 +22,10 @@
       $form.block({message: null,overlayCSS: {background: "#fff url(" + woocommerce_params.ajax_loader_url + ") no-repeat center",backgroundSize: "16px 16px",opacity: .6}});
 
       // Pass if we have a token
-      if( $form.find('[name=stripeToken]').length)
+      if( $form.find('[name=conektaToken]').length)
         return true;
 
-      Stripe.createToken($form, stripeResponseHandler)
-      // Prevent the form from submitting with the default action
+      Conekta.token.create($form,conektaSuccessResponseHandler, conektaErrorResponseHandler)
       return false;
     });
   });
