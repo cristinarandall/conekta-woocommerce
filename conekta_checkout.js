bba9c0f81818e813b2ea0da6ad76cea3486e4bbf
Cristina <cristina@conekta.mx>
2014-09-07 18:19:23 -0500

    tokenization error fixes

--- conekta_checkout.js
+++ conekta_checkout.js
@@ -1,16 +1,21 @@
  var initConektaCheckout = function(){
 
     jQuery(function($) {
+
     var $form = $('form.checkout,form#order_review');
            
     var conektaErrorResponseHandler  = function(response) {
            $form.find('.payment-errors').text(response.message);
            $form.unblock();
-             };       
+ 
+             };
+           
     var conektaSuccessResponseHandler = function(response) {
       $form.append($('<input type="hidden" name="conektaToken" />').val(response.id));
       $form.submit();
+
   };
+
     $('body').on('click', '#place_order,form#order_review input:submit', function(){
       if(jQuery('.payment_methods input:checked').val() !== 'ConektaCheckout')
       {
@@ -20,6 +25,8 @@
       Conekta.token.create($form, conektaSuccessResponseHandler, conektaErrorResponseHandler);
       return false;
     });
+
+
     $('body').on('click', '#place_order,form.checkout input:submit', function(){
       if(jQuery('.payment_methods input:checked').val() !== 'ConektaCheckout')
       {
@@ -27,14 +34,18 @@
       }
       $('form.checkout').find('[name=conektaToken]').remove()
     })
+
     $('form.checkout').bind('#place_order,checkout_place_order_ConektaCheckout', function(e){
       if($('input[name=payment_method]:checked').val() != 'ConektaCheckout'){
           return true;
       }
+
       $form.find('.payment-errors').html('');
       $form.block({message: null,overlayCSS: {background: "#fff url(" + woocommerce_params.ajax_loader_url + ") no-repeat center",backgroundSize: "16px 16px",opacity: .6}});
+
       if( $form.find('[name=conektaToken]').length)
         return true;
+
       Conekta.setPublishableKey($('#conekta_pub_key').data('publishablekey'));
       Conekta.token.create($form, conektaSuccessResponseHandler, conektaErrorResponseHandler);
       return false;
